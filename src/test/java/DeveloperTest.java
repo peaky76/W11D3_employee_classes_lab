88c2680cce6367a87077744401cb1944c89bb5f2
peaky76 <robertjamespeacock@gmail.com>
2020-08-26 15:13:02 +0100

    developer tests written

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Alina", "ZY987654X", 50000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Alina", developer.getName());
+    }
+
+    @Test
+    public void hasNI() {
+        assertEquals("ZY987654X", developer.getNI());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(50000, developer.getSalary());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        developer.raiseSalary(7000);
+        assertEquals(57000, developer.getSalary());
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(500, developer.payBonus(), 0.01);
+    }
+
 }
