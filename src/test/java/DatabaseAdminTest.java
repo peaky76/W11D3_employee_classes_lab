8cefc52d4697d3c7f027517f8eae6d7b2d46a42e
peaky76 <robertjamespeacock@gmail.com>
2020-08-26 15:17:45 +0100

    dataAdmin tests written

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before() {
+        databaseAdmin = new DatabaseAdmin("Rudy", "GH543219J", 40000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Rudy", databaseAdmin.getName());
+    }
+
+    @Test
+    public void hasNI() {
+        assertEquals("GH543219J", databaseAdmin.getNI());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(40000, databaseAdmin.getSalary());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        databaseAdmin.raiseSalary(8000);
+        assertEquals(48000, databaseAdmin.getSalary());
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(400, developer.payBonus(), 0.01);
+    }
+
 }
