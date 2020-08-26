36ba582ce25d5cc077fa500563284a566b962486
peaky76 <robertjamespeacock@gmail.com>
2020-08-26 15:14:23 +0100

    developer tests passed

--- src/main/java/techStaff/Developer.java
+++ src/main/java/techStaff/Developer.java
@@ -1,4 +1,10 @@
 package techStaff;
 
-public class Developer {
+import staff.Employee;
+
+public class Developer extends Employee {
+
+    public Developer(String name, String NI, int salary) {
+        super(name, NI, salary);
+    }
 }
