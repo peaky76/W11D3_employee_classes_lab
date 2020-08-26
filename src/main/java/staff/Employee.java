ebc4ced35a1a8a2e77003d6c14aee2b6ab7cbf0c
peaky76 <robertjamespeacock@gmail.com>
2020-08-26 14:51:22 +0100

    class init, getters

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -1,4 +1,33 @@
 package staff;
 
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private String NI;
+    private int salary;
+
+    public Employee(String name, String NI, int salary) {
+        this.name = name;
+        this.NI = NI;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNI() {
+        return NI;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double salaryIncrease) {
+        this.salary += salaryIncrease;
+    }
+
+    
+
 }
