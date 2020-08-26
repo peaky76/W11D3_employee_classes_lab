51e837a14cc2133ff3d97c63d0aeb0b12cf080ad
peaky76 <robertjamespeacock@gmail.com>
2020-08-26 15:27:46 +0100

    director tests passed

--- src/main/java/management/Director.java
+++ src/main/java/management/Director.java
@@ -1,4 +1,15 @@
 package management;
 
-public class Director {
+public class Director extends Manager {
+
+    private double budget;
+
+    public Director(String name, String NI, int salary, String deptName, double budget) {
+        super(name, NI, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
 }
